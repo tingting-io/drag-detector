7028d086ae4808c8ea1235ef92d3e5d0840f36a1
JingAn Chen <lzsoft.cja@gmail.com>
2017-04-26 18:23:29 -0400

    Temp.

--- src/drag-detector.js
+++ src/drag-detector.js
@@ -11,12 +11,32 @@ window.customElements.define('drag-detector', class extends window.HTMLElement {
         window.addEventListener("mousedown", (e) => {
             xs = e.screenX;
             ys = e.screenY;
+            document.body.classList.add("drag-detector-grab");
         });
         window.addEventListener("mouseup", (e) => {
             xe = e.screenX;
             ye = e.screenY;
             tail.call(this);
+            document.body.classList.remove("drag-detector-grab");
         });
+
+        function move(e) {
+            if (Math.abs(e.screenX - xs) < Math.abs(e.screenY - ys)) {
+                if (e.screenY - ys > tolerance) {
+                    document.body.style.cursor = "s-resize";
+                }
+                if (ys - e.screenY > tolerance) {
+                    document.body.style.cursor = "n-resize";
+                }
+            } else {
+                if (e.screenX - xs > tolerance) {
+                    document.body.style.cursor = "e-resize";
+                }
+                if (xs - e.screenX > tolerance) {
+                    document.body.style.cursor = "w-resize";
+                }
+            }
+        }
 
         function tail() {
             if (Math.abs(xe - xs) < Math.abs(ye - ys)) {
